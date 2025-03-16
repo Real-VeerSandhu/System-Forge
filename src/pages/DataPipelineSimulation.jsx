8fcc667a6083173485b0058145ebdc6318c8e558
Veer Sandhu <sandhu.s.veer@gmail.com>
2025-03-16 19:33:09 -0400

    send 2

--- src/pages/DataPipelineSimulation.jsx
+++ src/pages/DataPipelineSimulation.jsx
@@ -562,6 +562,7 @@ const DataPipelineSimulation = () => {
                   </div>
                 </div>
                 
+                
                 {/* Second row */}
                 <div className="col-span-4">
                   <div className="h-40 flex items-center justify-center">
